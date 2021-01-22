66bb0c389a9e379b544c9321bdab71bde5108e3b
Zaki_Dz <54380152+Zaki-Dz@users.noreply.github.com>
2021-01-22 14:52:11 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,3 +5,25 @@ setInterval(function(){
 	animation.style.display = 'none';
 
 }, 3000);
+
+let menu = document.querySelector('.menu');
+
+let menu_btn = document.querySelector('.menu-btn');
+
+let menu_click = false;
+
+menu_btn.addEventListener('click', function() {
+
+	if (menu_click == false) {
+
+		menu.style.transform = 'translateY(0)';
+		menu_click = true;
+
+	} else {
+
+		menu.style.transform = 'translateY(-100%)';
+		menu_click = false;
+
+	}
+
+});
